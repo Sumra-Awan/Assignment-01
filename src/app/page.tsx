feae8bdb09c4d071c4f8f829b8091c4ef0e9d9d7
Sumra-Awan <sumrajamil5@gmail.com>
2024-10-05 01:26:12 +0500

    I Created a NextJS app and coded a simple HELLO WORLD program in it.

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+
+
+
+
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',  
+        alignItems: 'center',       
+        height: '30vh',           
+        textAlign: 'center',        
+      }}
+    >
+      
+      <h1>HELLO WORLD!</h1>
+     
+    </div>
+  );
+}
+
+
+
+
+
+
